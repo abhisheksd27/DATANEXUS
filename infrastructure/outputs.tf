f529ca99cd5a7b60dd803c9bd2691f90a9f1cdec
abhisheksd27 <abhisheksdevadiga271@gmail.com>
2026-09-14 21:38:51 +0530

    n

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -20,7 +20,7 @@ output "bigquery_analytics_dataset" {
   value       = google_bigquery_dataset.analytics_dataset.dataset_id
 }
 
-output "pipeline_service_account_email" {
+output "pipeline_service_account_email" {.  
   description = "The email of the pipeline service account"
   value       = google_service_account.pipeline_sa.email
 }
